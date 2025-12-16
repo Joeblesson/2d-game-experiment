1437ea9af04238787e2659667dafa2723a4797ae
Joeblesson <johanblesson@gmail.com>
2025-12-16 19:27:44 +0100

    add plan.md

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include "function/homescreen.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    // preload (declare shapes here)
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "2D Game Experiment");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+    while (window.isOpen()) {
+        // loop to close window after use
+        while (const std::optional event = window.pollEvent()) {
+            // why tf is there an arrow?!
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
         }
 
         window.clear();
+
         window.draw(shape);
+        
         window.display();
     }
 }
---
+++ plan.md
@@ -0,0 +1,26 @@
+# plan for this game
+
+- level-based platformer / fighting game
+- boss levels
+- paint.net graphics
+- i guess like the first super mario game 
+- a map (maybe)
+- music on musescore :O
+
+name: Arctic Penguin (wip)
+
+
+# lore (kinda):
+
+penguin gets stranded in the arctic (shipping container)
+needs to get home to antarctica 
+different climates have different effects or something
+
+penguin need to find different things in different regions of the world
+to unlock a plane
+
+
+
+
+
+might be expanded upon idk
